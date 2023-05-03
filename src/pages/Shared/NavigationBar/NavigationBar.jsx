c1af12ed392de51afae6b24db54b8c3f5b3f1ea2
Sanjida Akter <sanjidakter2304@gmail.com>
2023-05-03 16:27:41 +0600

    active link done

--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -5,49 +5,74 @@ import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useMatch } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
+import './NavigationBar.css'
 
 const NavigationBar = () => {
-  const {user,logOut} = useContext(AuthContext);
-  const handleLogout = () =>{
-   logOut()
-   .then()
-   .catch(error=>console.log(error)
-    )
-  }
+  const location = useLocation();
+  const homeMatch = useMatch("/");
+  const blogMatch = useMatch("/blogs");
+
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogout = () => {
+    logOut()
+      .then()
+      .catch((error) => console.log(error));
+  };
   return (
-    <Container>
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-        <Container>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <nav>
-                <img style={{height:'64px',width:'80px'}} src={img} alt="" />
-            </nav>
-            <Nav className="mx-auto">
-              
-                <Link style={{textDecoration: 'none',marginRight:'20px'}} to="/">Home</Link>
-              
-              <Link style={{textDecoration: 'none'}} to="/blogs">Blog</Link>
-             
-            </Nav>
-          <Nav>
-            {
-              user?(
-                <Button onClick={handleLogout} variant="secondary"> {user?.displayName
-                } <img style={{borderRadius:'50%',width:'5%'}} src={user?.photoURL} alt="" /> </Button>
-              ):(
-                <Link to="/login">
-                <Button variant="secondary">Login</Button>
+    <div>
+      <Container>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Container>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <nav>
+                <img
+                  style={{ height: "64px", width: "80px" }}
+                  src={img}
+                  alt=""
+                />
+              </nav>
+              <Nav className="mx-auto">
+                <li className={homeMatch ? "active" : ""}>
+                  <Link
+                    style={{ textDecoration: "none", marginRight: "20px" }}
+                    to="/"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className={blogMatch ? "active" : ""}>
+                <Link style={{ textDecoration: "none" }} to="/blogs">
+                  Blog
                 </Link>
-              )
-            }
-          </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Container>
+                </li>
+
+                
+              </Nav>
+              <Nav>
+                {user ? (
+                  <Button onClick={handleLogout} variant="secondary">
+                    {" "}
+                    {user?.displayName}{" "}
+                    <img
+                      style={{ borderRadius: "50%", width: "5%" }}
+                      src={user?.photoURL}
+                      alt=""
+                    />{" "}
+                  </Button>
+                ) : (
+                  <Link to="/login">
+                    <Button variant="secondary">Login</Button>
+                  </Link>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
+    </div>
   );
 };
 
