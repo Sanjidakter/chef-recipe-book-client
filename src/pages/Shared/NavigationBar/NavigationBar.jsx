4c42ebd537001ae3002b896903e5c578d7c0af11
Sanjida Akter <sanjidakter2304@gmail.com>
2023-05-05 12:23:18 +0600

    responsive navbar

--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -23,87 +23,21 @@ const NavigationBar = () => {
       .catch((error) => console.log(error));
   };
   return (
-    <div>
-      {/* <Container >
-        <Navbar
-          style={{ width: "100%" }}
-          collapseOnSelect
-          expand="lg"
-          bg="light"
-          variant="light"
-        >
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <nav>
-              <img style={{ height: "64px", width: "80px" }} src={logo} alt="" />
-            </nav>{" "}
-            <p className="w-75"
-              style={{
-                fontWeight: "bold",
-                color: "black",
-                fontFamily: " sans-serif",
-              }}
-            >
-              
-              Chef Recipe Book
-            </p>
-            <Nav className="mx-auto w-75" style={{padding:'10%'}}>
-              <li className={homeMatch ? "active" : ""}>
-                <Link
-                  style={{ textDecoration: "none", marginRight: "20px" }}
-                  to="/"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className={blogMatch ? "active" : ""}>
-                <Link style={{ textDecoration: "none" }} to="/blogs">
-                  Blog
-                </Link>
-              </li>
-            </Nav>
-            <Nav style={{width:'40%'}}>
-            {user ? (
-                <Link to="/userdetail" >
-                  <img
-                    style={{ borderRadius: "50%", width: "80%", }}
-                    src={user?.photoURL}
-                    alt=""
-                    title={user?.displayName}
-                  />
-                </Link>
-              ) : (
-               
-                <Link to="/login">
-                  <Button variant="secondary">Login</Button>
-                </Link>
-              )}
-              <Link to="/login" style={{marginLeft:'10%',marginRight:'15%' }}>
-                <Button
-                  onClick={handleLogout}
-                  variant="secondary"
-                >
-                  Logout
-                </Button>
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </Container> */}
-
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            Chef Recipe Book
-          </Navbar.Brand>
-          <Nav className="">
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Chef Recipe Book
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
             <Nav>
               {" "}
               <li className={homeMatch ? "active" : ""}>
@@ -124,9 +58,13 @@ const NavigationBar = () => {
               </li>
             </Nav>
           </Nav>
-          <Nav>
+          <Nav className="ms-auto">
             {user ? (
-              <Link to="/userdetail" className="ms-auto" style={{width:'30%'}}>
+              <Link
+                to="/userdetail"
+                className="ms-auto"
+                style={{ width: "30%" }}
+              >
                 <img
                   width="30"
                   height="30"
@@ -147,9 +85,9 @@ const NavigationBar = () => {
               </Button>
             </Link>
           </Nav>
-        </Container>
-      </Navbar>
-    </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
